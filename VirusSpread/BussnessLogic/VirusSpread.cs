987bf9ea9ab5b49bd4dd4ec611d0b5503c0db855
DELL <DELL@CDG-6676-NB>
2023-09-08 15:22:52 +0800

    添加项目文件。

---
+++ VirusSpread/BussnessLogic/VirusSpread.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VirusSpread.BussnessLogic
+{
+    public class VirusSpread
+    {
+        public static ConcurrentBag<Model.PeopleModel.People> peoples;
+
+        public static void Init()
+        {
+            peoples = new ConcurrentBag<Model.PeopleModel.People>();
+            Random random = new Random(DateTime.Now.Millisecond * DateTime.Now.Second);
+            for (int i = 0; i < 100; i++)
+            {
+                Model.PeopleModel.People people = new Model.PeopleModel.People();
+                people.ID = i;
+                people.X = random.NextDouble() * 400;
+                people.Y = random.NextDouble() * 400;
+                people.Disease = false;
+                peoples.Add(people);
+            }
+            for (int i = 100; i < 103; i++)
+            {
+                Model.PeopleModel.People people = new Model.PeopleModel.People();
+                people.ID = i;
+                people.X = random.NextDouble() * 400;
+                people.Y = random.NextDouble() * 400;
+                people.Disease = true;
+                peoples.Add(people);
+            }
+        }
+        public static void Run(int spreadCount,int moveDistance)
+        {
+            int count = 0;
+            Random random = new Random(DateTime.Now.Millisecond * DateTime.Now.Second);
+            while (true)
+            {
+                if (count > spreadCount)
+                {
+                    break;
+                }
+                foreach (var item in peoples)
+                {
+                    int parity = random.Next(0,200);
+                    double tempX, tempY;
+                    if (parity % 2 == 0)
+                    {
+                        tempX = item.X + random.NextDouble() * moveDistance;
+                        tempY = item.Y + random.NextDouble() * moveDistance;
+                    }
+                    else
+                    {
+                        tempX = item.X - random.NextDouble() * moveDistance;
+                        tempY = item.Y - random.NextDouble() * moveDistance;
+                    }
+                    
+                    if (tempX>0 && tempX < 400 && tempY>0 && tempY < 400)
+                    {
+                        item.X = tempX;
+                        item.Y = tempY;
+                    }
+                    
+                }
+                Thread.Sleep(1000);
+                count++;
+            }
+        }
+        public static void Spread(int spreadCount,int infectious)
+        {
+            int count = 0;
+            while (true)
+            {
+                if (count > spreadCount)
+                {
+                    break;
+                }
+                var healthy = peoples.Where(item => item.Disease == false).ToList();
+                var infected = peoples.Where(item => item.Disease == true).ToList();
+
+
+                var pairsJoin = (from healthyItem in healthy
+                                 from infectedItem in infected
+                                 select new { healthyItem, infectedItem }).ToList();
+
+                foreach (var item in pairsJoin)
+                {
+                    CalcDistance(item.healthyItem, item.infectedItem, out double distance);
+                    if (distance < infectious)
+                    {
+                        var infectedLabel = peoples.Where(find => find.ID == item.healthyItem.ID).FirstOrDefault();
+                        infectedLabel.Disease = true;
+                    }
+                }
+                Thread.Sleep(1000);
+                count++;
+            }
+        }
+        public static void Display(out string statistics)
+        {
+            statistics = peoples.Where(item => item.Disease == true).Count().ToString();
+        }
+        public static void CalcDistance(Model.PeopleModel.People A, Model.PeopleModel.People B, out double distance)
+        {
+            double X = Math.Abs(A.X - B.X);
+            double Y = Math.Abs(A.Y - B.Y);
+            distance = Math.Sqrt(X * X - Y * Y);
+        }
+    }
+}
---
+++ VirusSpread/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static VirusSpread.Model.PeopleModel;
+
+namespace VirusSpread
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void begin_Click(object sender, RoutedEventArgs e)
+        {
+            int count = 10;
+            int moveDistance = int.Parse(moveSpeed.Text.ToString());
+            int infectiousNum = int.Parse(infectious.Text);
+            BussnessLogic.VirusSpread.Init();
+            GenPeople(BussnessLogic.VirusSpread.peoples);
+            Task.Run(() => { PeopleRun(BussnessLogic.VirusSpread.peoples, count); });
+            Task.Run(() => { BussnessLogic.VirusSpread.Run(count,moveDistance); });
+            Task.Run(() => { BussnessLogic.VirusSpread.Spread(count, infectiousNum); });
+        }
+        private void PeopleRun(ConcurrentBag<Model.PeopleModel.People> peoples,int count)
+        {
+            int spreadCount = 0;
+            while (true)
+            {
+                if (spreadCount> count)
+                {
+                    break;
+                }
+                map.Dispatcher.Invoke(new Action(() => {
+                    map.Children.Clear();
+                    int N = 0;
+                    foreach (var item in peoples)
+                    {
+                        Button button = buttonDic["P" + item.ID.ToString()];
+                        Canvas.SetLeft(button, item.X);
+                        Canvas.SetTop(button, item.Y);
+                        if (item.Disease)
+                        {
+                            button.Background = Brushes.Red;
+                            N++;
+                        }
+                        else
+                        {
+                            button.Background = Brushes.Green;
+                        }
+                        map.Children.Add(button);
+                    }
+                    infectedNum.Content = N.ToString();
+                    countLable.Content=spreadCount.ToString();
+                }));
+                Thread.Sleep(1000);
+                spreadCount++;
+            }
+        }
+        ConcurrentDictionary<string, Button> buttonDic;
+        private void GenPeople(ConcurrentBag<Model.PeopleModel.People> peoples)
+        {
+            buttonDic = new ConcurrentDictionary<string, Button>();
+            map.Children.Clear();
+            foreach (var item in peoples)
+            {
+                Button button = new Button();
+                button.Name = "P" + item.ID.ToString();
+                button.Content = item.ID.ToString();
+                button.Foreground = Brushes.White;
+                button.FontSize = 6;
+                button.Width = 15;
+                button.Height = 15;
+                Canvas.SetLeft(button,item.X);
+                Canvas.SetTop(button, item.Y);
+                if (item.Disease)
+                {
+                    button.Background = Brushes.Red;
+                }
+                else 
+                {
+                    button.Background = Brushes.Green;
+                }
+                map.Children.Add(button);
+                buttonDic.TryAdd(button.Name,button);
+            }
+        }
+    }
+}
